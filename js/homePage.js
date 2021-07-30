edd5084ed245382040b1956f4447ec4ea13c9682
kaushik <kaushik.393@gmail.com>
2021-07-31 02:09:51 +0530

    added dynamic fetch works

--- js/homePage.js
+++ js/homePage.js
@@ -1,5 +1,7 @@
 $('document').ready(function(){
     countDown();
+    fetchWeather();
+    fetchDestinations();
 }) 
 
 
@@ -29,4 +31,35 @@ function countDown() {
         }
     });
 
-};
+}
+
+function fetchWeather() {
+
+    $.ajax({url: "https://run.mocky.io/v3/e3ae9d2e-78f5-403d-b6cd-fa7f8c7e1576", success: function(result){
+       
+    for(var i=0; i<=result.result.length; i++){
+        var $ele = '<div id='+i+'><div>"'+result.result[i].city+'"</div><br>'+
+                    '<img alt="image"/>'+
+                    '<div>"'+result.result[i].temp_Celsius+'"</div></div>'
+
+$(".siriusHome_weather").append($ele)
+    }
+        console.log(result)
+      }});
+ 
+}
+
+function fetchDestinations() {
+
+    $.ajax({url: "https://run.mocky.io/v3/3e6901dd-9a60-4771-a8cb-9c62177a654c", success: function(result){
+       
+    for(var i=0; i<=result.result.length; i++){
+        var $ele = '<div id='+i+'><div>"'+result.result[i].city+'"</div><br>'+
+                    '<img src='+result.result[i].imageUrl+' alt="image"/>';
+
+$(".siriusHome_destination").append($ele)
+    }
+        console.log(result)
+      }});
+ 
+}
